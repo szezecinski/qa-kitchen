9cb63cd2e1cfdd99ff5cdf2ba177e0bf68835de6
Fernando Leite Szezecinski <szezecinski1@gmail.com>
2025-12-23 09:31:02 -0300

    refactor: Remove unused React import from icons
    The React import was not being used in any of the icon components, so it has been removed to clean up the code.

--- components/icons.tsx
+++ components/icons.tsx
@@ -1,6 +1,4 @@
 
-import React from 'react';
-
 export const EyeIcon = () => (
   <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
